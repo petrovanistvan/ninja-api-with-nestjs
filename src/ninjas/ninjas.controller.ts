357f9bafe3618b8aa712420331639292914aa701
Istvan Petrovan <petrovanistvan@gmail.com>
2023-03-26 13:08:08 +0200

    added crud routes

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+} from '@nestjs/common';
+
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas?type=fast --> []
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  // GET /ninjas/:id --> { ... }
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  // POST /ninjas
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  // PUT /ninjas/:id --> { ... }
+  @Put(':id')
+  updateNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  removeNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+}
